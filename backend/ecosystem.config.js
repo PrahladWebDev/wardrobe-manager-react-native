1d21e254daf3925ca5050e1997ebfc367bb69f3b
Prahlad Singh <prahladsingh7512@gmail.com>
2026-08-29 23:52:10 +0530

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,13 +3,14 @@ module.exports = {
     {
       name: 'wardrobe-api',
       script: 'server.js',
-      cwd: '/home/prahlad/wardrobe-manager/backend',
+      cwd: '/var/www/projects/wardrobe/backend',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
       env_production: {
         NODE_ENV: 'production',
+        PORT: 5009,
       },
     },
   ],
